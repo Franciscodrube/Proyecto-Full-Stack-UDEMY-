8f802da4d0e822f03288e13525edb14b4368e003
franciscodrube <franciscodrube@gmail.com>
2022-05-18 21:11:16 -0300

    Cambio de llamar con funciones a EventListener.

--- Figuras_geométricas/geometrias.js
+++ Figuras_geométricas/geometrias.js
@@ -1,39 +1,46 @@
 //Patrón Modular (()=>{ CODIGO })();
-(()=>{
+const myModule = (()=>{
 
-    
+    'use strict'
     let valueResult = document.getElementById("result");
-     //area y perimetro de un cuadrado
+    const   btnPerimSq = document.getElementById("btnPerimSq"),
+            btnAreaSq = document.getElementById("btnAreaSq"),
+            btnPerimTri = document.getElementById("btnPerimTri"),
+            btnAreaTri = document.getElementById("btnAreaTri"),
+            btnDiamCr = document.getElementById("btnDiamCr"),
+            btnAreaCr = document.getElementById("btnAreaCr"),
+            btnPerimCr = document.getElementById("btnPerimCr");
+
+
+    // area y perimetro de un cuadrado
     let perimSquare = (side) => side * 4;
     let areaSquare = (side) => side * side;
+
+
+   btnPerimSq.addEventListener("click",()=> {
+        let imputSide = document.getElementById("imputSide");
+        let value = Number(imputSide.value);
+
+        let result = `El perímetro del cuadrado es: ${perimSquare(value)} cm`;
+        valueResult.innerText = result;
+    });
+
+    btnAreaSq.addEventListener("click", ()=>{
+    
+        let imputSide = document.getElementById("imputSide");
+        let value = Number(imputSide.value);
+
+        let result = `El area del cuadrado es: ${areaSquare(value)} cm cuadrados`;
+        valueResult.innerText = result;
+    });
     
      //area y perimetro de un triangulo
 
     let perimTriangle = (side1,side2,side3) => side1+side2+side3;
     let areaTriangle =(base,altura) => base*altura;
 
-    //Diametro area y perimetro de un circulo
-
-    let diamCircle = (radio) => radio*2;
-    let perimCircle = (radio) => radio*2* 3.1416;
-    let areaCircle = (radio) => radio* radio * 3.1416;
-
-
-    function calculatePerimSquare(){
-        let imputSide = document.getElementById("imputSide");
-        let value = Number(imputSide.value);
-
-        let result = `El perímetro del cuadrado es: ${perimSquare(value)} cm`;
-        valueResult.innerText = result;
-    }
-    function calculateAreaSquare(){
-        let imputSide = document.getElementById("imputSide");
-        let value = Number(imputSide.value);
-
-        let result = `El area del cuadrado es: ${areaSquare(value)} cm cuadrados`;
-        valueResult.innerText = result;
-    }
-    function calculatePerimTriangle(){
+    btnPerimTri.addEventListener("click", ()=>{
+   
         let imputSide1 = document.getElementById("lado1-triangulo"); 
         let value1 = Number(imputSide1.value);
         let imputSide2 = document.getElementById("lado2-triangulo"); 
@@ -43,9 +50,10 @@
 
         let result = `El perimetro del triángulo es: ${perimTriangle(value1,value2,value3)} cm cuadrados`;
         valueResult.innerText = result;
-    }
+    });
 
-    function calculateAreaTriangle(){
+    btnAreaTri.addEventListener("click", ()=>{
+   
         let baseTri = document.getElementById("base-triangulo");
         let value1 = Number(baseTri.value);
         let alturaTri = document.getElementById("altura-triangulo");
@@ -54,35 +62,54 @@
         let result = `El area del triángulo es: ${areaTriangle(value1,value2)} cm`;
         valueResult.innerText = result;
 
-    }
+    });
 
-    function calculateDiamCircle(){
+    //Diametro area y perimetro de un circulo
+
+    let diamCircle = (radio) => radio*2;
+    let perimCircle = (radio) => radio*2* 3.1416;
+    let areaCircle = (radio) => radio* radio * 3.1416;
+
+
+    btnDiamCr.addEventListener("click", ()=>{
+   
         let radio = document.getElementById("radio");
         let value = Number(radio.value);
 
-    
         let result = `El diametro del circulo es: ${diamCircle(value)} cm`;
         valueResult.innerText = result;
-    }
+    });
 
-    function calculateAreaCircle(){
+    btnAreaCr.addEventListener("click", ()=>{
+   
         let radio = document.getElementById("radio");
         let value = Number(radio.value);
 
-    
         let result = `El area del circulo es: ${areaCircle(value)} cm`;
         valueResult.innerText = result;
-    }
+    });
 
-    function calculatePerimCircle(){
+    btnPerimCr.addEventListener("click", ()=>{
+   
         let radio = document.getElementById("radio");
         let value = Number(radio.value);
 
     
         let result = `El perímetro del circulo es: ${perimCircle(value)} cm`;
         valueResult.innerText = result;
-    }
+    });
 
+    // return {
+    //     cpsq : calculatePerimSquare,
+    //     casq : calculateAreaSquare,
+        
+    //     cpc : calculatePerimCircle,
+    //     cac : calculateAreaCircle,
+    //     cdc : calculateDiamCircle,
+
+    //     cpt : calculatePerimTriangle,
+    //     cat : calculateAreaTriangle,
+    // }
 })();
 
 
